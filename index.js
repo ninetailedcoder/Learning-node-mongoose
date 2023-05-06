ea2881f39246e028c5e6578edec487146384cd2c
Tory DeMaio <torydemaio@gmail.com>
2023-05-05 20:31:58 -0500

    Mongoose part 2

--- index.js
+++ index.js
@@ -11,25 +11,23 @@ const connect = mongoose.connect(url, {
 connect.then(() => {
     console.log('Connected correctly to server');
 
-    const newCampsite = new Campsite({
+    Campsite.create({
         name: 'React Lake Campground',
         description: 'test'
+    })
+    .then(campsite => {
+        console.log(campsite);
+        return Campsite.find();
+    })
+    .then(campsites => {
+        console.log(campsites);
+        return Campsite.deleteMany();
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch(err => {
+        console.log(err);
+        mongoose.connection.close();
     });
-
-    newCampsite.save()
-        .then(campsite => {
-            console.log(campsite);
-            return Campsite.find();
-        })
-        .then(campsites => {
-            console.log(campsites);
-            return Campsite.deleteMany();
-        })
-        .then(() => {
-            return mongoose.connection.close();
-        })
-        .catch(err => {
-            console.log(err);
-            mongoose.connection.close();
-        });
 })
